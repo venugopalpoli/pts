7eecd8138125cb0c3c0859b415180067d6ff7ada
venugopalpoli <venugopal.poli@gmail.com>
2015-07-09 10:55:43 +0100

    This is my first class

---
+++ MyFirstProject/src/com/test/first/TestClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test.first;
+
+/**
+ * @author vpoli
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("welocome");
+	}
+
+}
