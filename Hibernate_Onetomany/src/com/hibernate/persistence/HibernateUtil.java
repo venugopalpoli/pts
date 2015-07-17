5edc41272a7cea5e1fcc31a6f98653b7e55724cb
venugopalpoli <venugopal.poli@gmail.com>
2015-07-17 21:23:51 +0100

    removed sapces

--- Hibernate_Onetomany/src/com/hibernate/persistence/HibernateUtil.java
+++ Hibernate_Onetomany/src/com/hibernate/persistence/HibernateUtil.java
@@ -13,9 +13,7 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
-    
-    
-    
+     
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
