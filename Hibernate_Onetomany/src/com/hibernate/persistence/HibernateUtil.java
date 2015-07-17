cc9796379fa19aae2a2097e194da71d9a35ffeae
venugopalpoli <venugopal.poli@gmail.com>
2015-07-17 21:22:15 +0100

    Added spaces

--- Hibernate_Onetomany/src/com/hibernate/persistence/HibernateUtil.java
+++ Hibernate_Onetomany/src/com/hibernate/persistence/HibernateUtil.java
@@ -14,6 +14,8 @@ public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
     
+    
+    
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
